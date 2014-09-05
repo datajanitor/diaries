b3d37a835d4409b15ef1f945316d473ddf407268
Dan Nguyen <dansonguyen@gmail.com>
2014-09-04 21:20:25 -0700

    coming up with a convention

--- dallas_crime_reports/lib/dallas_crime_reports.rb
+++ dallas_crime_reports/lib/dallas_crime_reports.rb
@@ -1,3 +1,4 @@
+require 'pathname'
 module DallasCrimeReports
 
 # TODO: Workspace
@@ -5,4 +6,34 @@ module DallasCrimeReports
 # TODO: Unpacking
 # TODO: Munging
 
+  DATA_DIR = Pathname.new File.expand_path( '../../data-hold', __FILE__)
+  def self.setup!
+    %w(fetched unpacked munged).each do |dname|
+      DATA_DIR.join(dname).mkpath
+    end
+  end
+
+  module Fetching
+    def fetch!
+      require 'net/ftp'
+      ftp = Net::FTP.new('66.97.146.93')
+      ftp.login
+      ftp.nlst('*.zip').each do |zipname|
+        puts "Downloading #{zipname}"
+        ftp.getbinaryfile(zipname, DATA_DIR.join('fetched', File.basename(zipname))
+        sleep rand(5) + 1
+      end
+    end
+  end
+
+  module Unpacking
+    require 'zip'
+    # unzip files into unpacked
+  end
+
+  module Munging
+    def munge!
+
+    end
+  end
 end
