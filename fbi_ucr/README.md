11a259b7d34b02dbed8227e43170d216e347f940
dantnguyen <dan@danwin.com>
2014-09-16 06:14:52 -0700

    more persistent

--- fbi_ucr/README.md
+++ fbi_ucr/README.md
@@ -4,4 +4,15 @@ There seems to be a lack of bulk data downloads of the important FBI Uniform Cri
 
 Their tablemaker tool is here: http://www.ucrdatatool.gov/Search/Crime/Local/JurisbyJuris.cfm
 
-The `data-source/key-state.csv` file contains the numbers (1 to 51) assigned to each state.
+
+The fetching method is straightforward. Use the [Ruby mechanize gem](http://readysteadycode.com/howto-scrape-websites-with-ruby-and-mechanize) to fill out the forms. You could probably just examine the POST request and skip the form process...but it's pretty straightforward to do it this old fashioned way.
+
+There is also a POST request available for CSV files:
+
+    http://www.ucrdatatool.gov/Search/Crime/Local/DownCrimeJurisbyJuris.cfm/LocalCrimeJurisbyJuris.csv
+    
+
+An example parameter list for that request:
+
+    CrimeCrossId=4060&YearStart=1985&YearEnd=2012&DataType=1%2C2%2C3%2C4
+
--- fbi_ucr/lib/fbi_ucr.rb
+++ fbi_ucr/lib/fbi_ucr.rb
@@ -10,7 +10,7 @@ module FbiUcr
     end
 
     def fetch!
-      Fetching.scrape_form
+      Fetching.scrape_forms
     end
 
     def unpack!
@@ -23,27 +23,62 @@ module FbiUcr
   end
 
   module Fetching
-    FORM_START_URL = 'http://www.ucrdatatool.gov/Search/Crime/Local/LocalCrime.cfm'
-    KEY_STATE_DATAFILE = File.expand_path('../../data-source/key-state.csv', __FILE__)
+    STARTING_URL = 'http://www.ucrdatatool.gov/Search/Crime/Local/JurisbyJuris.cfm'
     class << self
-      def scrape_form
+      def scrape_forms
         require 'open-uri'
         require 'mechanize'
+        agent = Mechanize.new
+        # this needed to prevent Net::HTTP::Persistent::Errors
+        # https://github.com/sparklemotion/mechanize/issues/123
+        agent.read_timeout = 60
+        # fetch the first form
+        agent.get STARTING_URL
+        form_for_states = agent.page.forms[0]
 
-        # iterate through KEY_STATE_DATAFILE
-
-        # iterate through each agency per state
-
-        # collect the table HTML for each agency
-      end
-
-      private
-        # form is a Mechanize::Form with values to be filled out
-        # a copy of the form is made, with values filled in
-        def fill_agency_form(form, agency_id)
-
+        form_for_states.field_with(name: 'StateId').options.each do |state_opt|
+          state_opt.select  # this sets the state field
+          puts "State: #{state_opt.text.strip} - #{state_opt.value}"
+          form_for_states.submit
+          # now we're on page 2
+          form_for_reported_crimes = agent.page.forms[0]
+          # first, let's set the field for agency
+          # i.e. a. Choose an agency:
+          agency_options = form_for_reported_crimes.field_with(name: 'CrimeCrossId').options
+          agency_options.each do |agency_opt|
+            # select the current agency
+            agency_opt.select
+            # prep the file
+            fname = DATA_DIR.join('fetched', "#{agency_opt.value}.html")
+            # skip if file already exists
+            if fname.exist?
+              puts "Already downloaded: #{agency_opt.text.strip}: #{agency_opt.value}"
+              next
+            end
+            #  select all of the (4) variable groups
+            form_for_reported_crimes.field_with(name: 'DataType').options.each{|o| o.select }
+            # submit the form
+            # because there are two submit buttons, we want to pick the correct one
+            btn_submit = form_for_reported_crimes.button_with(value: 'Get Table')
+            # click the submit button
+            form_for_reported_crimes.click_button( btn_submit )
+            # now that agent.page has the current data, write it to file as text
+            open(fname, 'w'){|f| f.write agent.page.parser.to_html }
+            # sleepy time
+            puts "Downloaded #{agency_opt.text.strip}: #{agency_opt.value}"
+            sleep 0.5 + rand(3)
+          end
+          # example output:
+          # State: Alabama - 1
+          # Downloading Alabaster Police Dept: 102
+          # Downloading Albertville Police Dept: 104
+          # Downloading Alexander City Police Dept: 105
+          # Downloading Anniston Police Dept: 110
+          # Downloading Athens Police Dept: 119
+          # Downloading Atmore Police Dept: 120
+          # Downloading Auburn Police Dept: 122
         end
+      end
     end
   end # module Fetching
-
 end
