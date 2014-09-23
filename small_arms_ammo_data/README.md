1b7957793c3f912106ff7f67cf83ec0eb72ac56d
Dan Nguyen <dansonguyen@gmail.com>
2014-09-22 17:48:52 -0700

    small arms

---
+++ small_arms_ammo_data/README.md
@@ -0,0 +1,5 @@
+# Small arms data exports/imports
+ 
+This data is taken from Google's Data Arts Team's [ArmsGlobe](http://armsglobe.chromeexperiments.com/) viz. Their repo can be found here: https://github.com/dataarts/armsglobe
+
+The cleaning script simply converts the JSON into CSV
--- small_arms_ammo_data/convert_to_csv.rb
+++ small_arms_ammo_data/convert_to_csv.rb
@@ -2,17 +2,25 @@ require 'json'
 require 'csv'
 DATA_FILE = File.expand_path('../data-source/all.json', __FILE__)
 CSV_FILE =  File.expand_path('../data-munged/all.csv', __FILE__)
+USA_EXPS_CSV_FILE =  File.expand_path('../data-munged/usa-exports.csv', __FILE__)
+USA_IMPS_CSV_FILE =  File.expand_path('../data-munged/usa-imports.csv', __FILE__)
 
+csv = CSV.open(CSV_FILE, 'w')
+usa_exp_csv = CSV.open(USA_EXPS_CSV_FILE, 'w')
+usa_imp_csv = CSV.open(USA_IMPS_CSV_FILE, 'w')
 
 timebins = JSON.parse(open(DATA_FILE){|f| f.read })['timeBins']
-CSV.open(CSV_FILE, 'w') do |csv|
-  csv << %w(year importer category exporter value)
 
-  timebins.each do |t|
-    year = t['t']
-    data = t['data']
-    puts "#{year}: #{data.count} entries"
-
-    data.each{|d| csv << [year, d['i'], d['wc'], d['e'], d['v']] }
+[csv, usa_exp_csv, usa_imp_csv ].each{ |c| c << %w(year importer category exporter value) }
+timebins.each do |t|
+  year = t['t']
+  data = t['data']
+  puts "#{year}: #{data.count} entries"
+  data.each do |d|
+    row = [year, d['i'], d['wc'], d['e'], d['v']]
+    csv <<  row
+    usa_exp_csv << row if d['e'] == 'United States'
+    usa_imp_csv << row if d['i'] == 'United States'
   end
 end
+
