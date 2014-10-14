69a59e8199a9132feac94434fa38a6352f771cf3
Dan Nguyen <dansonguyen@gmail.com>
2014-10-14 13:30:15 -0700

    more babies

--- ssa_state_baby_names/lib/ssa_all_baby_names.rb
+++ ssa_state_baby_names/lib/ssa_all_baby_names.rb
@@ -1,40 +1,142 @@
-# just drafting out some things
 require 'pathname'
-require 'csv'
-csv = CSV.open('all-compiled-names.csv', 'w', headers: true)
-csv << [:year, :name, :sex, :count, :count_change, :count_per_100k, :count_per_100k_change, :rank, :rank_change]
+module SsaAllBabyNames
+  DATA_DIR = Pathname.new File.expand_path( '../../data-hold', __FILE__)
 
-files = Pathname.new File.expand_path("../../data-hold/names/*.txt", __FILE__)
-prev_year = {}
-Dir.glob(files).each do |file|
-  puts file
-  rows = open(file){|f| f.readlines.map{|r| r.strip.split(',').tap{|s| s[2] = s[2].to_i } }  }
-  year = File.basename(file)[/\d{4}/]
-  total_babies = rows.reduce(0){ |s,x| s += x[2] }
-  prev_name = nil
-  c_rank = 0
-  this_year = {}
-
-  rows.each do |row|
-    h = {:year => year}
-    h[:name], h[:sex], h[:count] = row
-    h[:count_per_100k] = (100000.0 * h[:count] / total_babies).round
-    c_rank += 1 if prev_name.nil? || prev_name[:count] > h[:count]
-    h[:rank] = c_rank
-    if (x = prev_year[[h[:name], h[:sex]]])
-      h[:rank_change] = x[:rank] - h[:rank]
-      h[:count_change] = h[:count] - x[:count]
-      h[:count_per_100k_change] = h[:count_per_100k] - x[:count_per_100k]
-    else
-      h[:rank_change] = h[:count_change] = h[:count_per_100k_change] = 0
+  class << self
+    def setup!
+      %w(fetched unpacked munged).each do |dname|
+        DATA_DIR.join(dname).mkpath
+      end
     end
 
-    csv << h
-    prev_name = h
-    this_year[[h[:name], h[:sex]]] = h
+    def fetch!
+      Fetching.fetch_from_website
+    end
+
+    def unpack!
+      Unpacking.unzip
+    end
+
+    def munge!
+      Munging.combine
+    end
   end
-  prev_year = this_year
-  this_year = {}
+
+
+
+  module Fetching
+    class << self
+      def fetch_from_website
+        require 'open-uri'
+        remote_url = 'http://www.ssa.gov/oact/babynames/names.zip'
+        file = DATA_DIR.join('fetched', 'all-names.zip')
+        open(file, 'w') do |f|
+          f.write open(remote_url){|o| o.read}
+        end
+      end
+    end
+  end # Fetching
+
+  module Unpacking
+    class << self
+      def unzip
+        require 'open-uri'
+        require 'zip'
+        zipname = DATA_DIR.join('fetched', 'all-names.zip')
+        alldir = DATA_DIR.join('unpacked', 'all')
+        alldir.mkpath
+        Zip::File.open(zipname) do |z|
+          z.select{|e| e.name =~ /yob\d{4}/}.each do |entry|
+            dest_fname = alldir.join  entry.name
+            puts "Extracting to #{dest_fname}"
+            # this strange #extract notation simply means we overwrite existing files
+            entry.extract(dest_fname){ true }
+          end
+        end
+      end
+      true
+    end
+  end # unpacking
+
+
+  # Each file looks like this:
+
+  #   Mary,F,8148
+  #   Anna,F,3143
+  #   Emma,F,2303
+  #   Elizabeth,F,2187
+  #   Minnie,F,2004
+
+  module Munging
+    class << self
+      def combine
+        require 'csv'
+        # set up the CSV file
+        csv_name = DATA_DIR.join('munged', 'all-names.csv')
+        csv = CSV.open(csv_name, 'w', headers: true)
+        csv << [:year, :name, :sex, :count, :per_100k, :rank,  :count_change, :per_100k_change, :rank_change]
+
+        # set up a placeholder for prev_year
+        prev_year_data = {}
+        # gather up the unpacked files, which each represent a year
+        fnames = Dir.glob DATA_DIR.join('unpacked', 'all', '*.txt')
+        fnames.each do |fname|
+          current_year_data = {}
+          year = File.basename(fname)[/\d{4}/]
+          puts "#{year}"
+          rows = open(fname){|f| f.readlines.map{|r| r.strip.split(',').tap{|s| s[2] = s[2].to_i } }  }
+
+          # group rows by name and sex
+          rows_grouped_by_sex = rows.group_by{ |row| row[1]}
+          rows_grouped_by_sex.each_pair do |sex, sex_rows|
+            puts "\t#{sex}"
+            last_rank = 0
+            last_baby_count = 0
+            total_names_of_sex = sex_rows.count
+            total_babies_of_sex = sex_rows.inject(0){|sum, r| sum += r[2] }
+            sex_rows.each_with_index do |row, idx|
+              baby = {
+                year: year,
+                name: row[0],
+                sex: row[1],
+                count: row[2]
+              }
+              # calculate rank
+              if last_baby_count == baby[:count]
+                # no increment in rank
+                baby[:rank] = last_rank
+              else
+                # not a tie
+                last_rank = baby[:rank] = (idx + 1)
+                last_baby_count = baby[:count]
+              end
+              # calculate per_100k
+              baby[:per_100k] = (baby[:count] * 100000.0 / total_babies_of_sex).round
+              # if previous year data exists, calculate the change
+              if (p_baby = prev_year_data[[baby[:name], baby[:sex]]])
+                baby[:count_change] = baby[:count] - p_baby[:count]
+                baby[:per_100k_change] = baby[:per_100k] - p_baby[:per_100k]
+                baby[:rank_change] = p_baby[:rank] - baby[:rank]
+              else
+                baby[:count_change] = baby[:count]
+                baby[:per_100k_change] = baby[:per_100k]
+                # need to invert this number by total_names
+                baby[:rank_change] = (total_names_of_sex - baby[:rank]) + 1
+              end
+              # write to CSV
+              csv << baby
+              current_year_data[[baby[:name], baby[:sex]]] = baby
+            end # done with this sex
+          end # done with this year / fname
+
+          prev_year_data  = current_year_data
+        end
+
+        csv.close
+        true
+      end
+    end # munging
+
+  end
 end
 
-csv.close
--- ssa_state_baby_names/lib/ssa_state_baby_names.rb
+++ ssa_state_baby_names/lib/ssa_state_baby_names.rb
@@ -1,87 +1,148 @@
-require 'fileutils'
-require 'zip'
-require 'open-uri'
-module SSAStateBabyNames
-  DATA_SOURCE_URL = 'http://www.ssa.gov/oact/babynames/state/namesbystate.zip'
-
-  PROCESSED_DATA_DIR = File.expand_path('../../data-hold/processed', __FILE__)
-  RAW_DATA_DIR = File.expand_path('../../data-hold/raw', __FILE__)
-  FileUtils.mkdir_p PROCESSED_DATA_DIR
-  FileUtils.mkdir_p RAW_DATA_DIR
-
+require 'pathname'
+module SsaStateBabyNames
+  DATA_DIR = Pathname.new File.expand_path( '../../data-hold', __FILE__)
   class << self
-    def fetch_and_unzip
-      zipname = File.join RAW_DATA_DIR, File.basename(DATA_SOURCE_URL)
-      # fetch from the URL and save to disk
-      puts "Downloading from #{DATA_SOURCE_URL}"
-      open(zipname, 'wb'){|f| f.write open(DATA_SOURCE_URL){|u| u.read } }
-      # now unzip
-      Zip::File.open(zipname) do |zip_file|
-        zip_file.each do |entry|
-          dest_fname = File.join RAW_DATA_DIR, entry.name
-          puts "Extracting to #{dest_fname}"
-          # this strange #extract notation simply means we overwrite existing files
-          entry.extract(dest_fname){ true }
-        end
+    def setup!
+      %w(fetched unpacked munged).each do |dname|
+        DATA_DIR.join(dname).mkpath
       end
-      # return a list of text files
-      Dir.glob(File.join RAW_DATA_DIR, '*.TXT')
     end
 
-    def combine
-      require 'csv'
-      csv = CSV.open(File.join(PROCESSED_DATA_DIR, 'all-states.csv'), 'w')
-      csv << %w(state sex year name count count_per_100k rank count_change count_per_100k_change rank_change)
-      txtfiles = Dir.glob(File.join RAW_DATA_DIR, '*.TXT')
-      txtfiles.each do |txt_name|
-        puts "Adding #{File.basename txt_name}"
-        # normally, loading an entire CSV into memory, converting to an array
-        # and then doing a group_by is not advisable. But these files
-        # are pretty small and so this operation shouldn't crash most modern
-        # computers. But yes, I'm being lazy here
-        grouped_rows = CSV.open(txt_name).to_a.group_by{ |row| [row[1], row[2]] }
-        # grouped_rows is a Hash with year values for keys
-        prev_year = {}
-        grouped_rows.each_value do |rows|
-          total_names = rows.count
-          total_babies = rows.inject(0){|sum, r| sum += r[4].to_i }
-          last_rank = 1
-          last_baby_count = 0
-          current_year = rows.each_with_index.inject({}) do |cy_hsh, (row, idx)|
-            current_baby_count = row[4].to_i
-            if last_baby_count == current_baby_count
-              # this is a tie, so rank should be the same as the last
-              rank = last_rank
-            else
-              # this is not a tie
-              last_rank = rank = idx + 1
-              last_baby_count = current_baby_count
-            end
+    def fetch!
+      Fetching.fetch_from_website
+    end
 
-            per_100k = (current_baby_count * 100000.0 / total_babies).round            # calculate change from last year
-            p_row = prev_year[row[3]]
-            if p_row.nil?
-              count_change = 0
-              rank_change = 0
-              per_100k_change = 0
-            else
-              count_change = current_baby_count - p_row[0]
-              rank_change =  p_row[2] - rank  # we want the inverse of the difference
-              per_100k_change = per_100k - p_row[1]
-            end
-            csv << (row << per_100k << rank << count_change <<  per_100k_change << rank_change )
-            # use name field as the key
-            cy_hsh[row[3]] = [current_baby_count, per_100k, rank]
+    def unpack!
+      Unpacking.unzip
+    end
 
-            cy_hsh
-          end
-          prev_year = current_year
-        end
-      end # end of txtfiles.each
-
-      csv.close
+    def munge!
+      Munging.combine
     end
   end
 
+  module Fetching
+    class << self
+      def fetch_from_website
+        require 'open-uri'
+        remote_url = 'http://www.ssa.gov/oact/babynames/state/namesbystate.zip'
+        file = DATA_DIR.join('fetched', 'namesbystate.zip')
+        open(file, 'w') do |f|
+          f.write open(remote_url){|o| o.read}
+        end
+      end
+    end
+  end # Fetching
 
+  module Unpacking
+    class << self
+      def unzip
+        require 'open-uri'
+        require 'zip'
+        zipname = DATA_DIR.join('fetched', 'namesbystate.zip')
+        state_dir = DATA_DIR.join 'unpacked', 'states'
+        state_dir.mkpath
+        Zip::File.open(zipname) do |z|
+          z.select{|e| e.name =~ /[A-Z]{2}\.TXT/}.each do |entry|
+            dest_fname = state_dir.join entry.name
+            puts "Extracting to #{dest_fname}"
+            # this strange #extract notation simply means we overwrite existing files
+            entry.extract(dest_fname){ true }
+          end
+        end
+        true
+
+      end
+    end
+  end # unpacking
+
+
+  # Each file looks like this:
+
+  #   AK,F,1910,Mary,14
+  #   AK,F,1910,Annie,12
+  #   AK,F,1910,Anna,10
+  #   AK,F,1910,Margaret,8
+  #   AK,F,1910,Helen,7
+  #   AK,F,1910,Elsie,6
+
+  module Munging
+    class << self
+      def combine
+        require 'csv'
+        # set up the CSV file
+        csv_name = DATA_DIR.join('munged', 'state-names.csv')
+        csv = CSV.open(csv_name, 'w', headers: true)
+        csv << [:state, :year, :name, :sex, :count, :per_100k, :rank,  :count_change, :per_100k_change, :rank_change]
+
+        # set up a placeholder for prev_year
+
+        # gather up the unpacked files, which each represent a year
+        fnames = Dir.glob DATA_DIR.join('unpacked', 'states', '*.TXT')
+        fnames.each do |fname|
+          current_year_data = {}
+          puts fname
+          rows = open(fname){|f| f.readlines.map{|r| r.strip.split(',').tap{|s| s[4] = s[4].to_i } }  }
+
+          # first, we break things up by year
+          prev_year_data = {}
+          rows_grouped_by_year = rows.group_by{|row| row[2]}
+          rows_grouped_by_year.each_pair do |year, year_rows|
+            puts year
+            # group rows by name and sex
+            rows_grouped_by_sex = year_rows.group_by{ |row| row[1] }
+            rows_grouped_by_sex.each_pair do |sex, sex_rows|
+              puts "\t#{sex}"
+              total_names_of_sex = sex_rows.count
+              total_babies_of_sex = sex_rows.inject(0){|sum, r| sum += r[4] }
+              last_rank = 0
+              last_baby_count = 0
+
+
+              sex_rows.each_with_index do |row, idx|
+                baby = {
+                  state: row[0],
+                  year: year,
+                  name: row[3],
+                  sex: sex,
+                  count: row[4]
+                }
+                # calculate rank
+                if last_baby_count == baby[:count]
+                  # no increment in rank
+                  baby[:rank] = last_rank
+                else
+                  # not a tie
+                  last_rank = baby[:rank] = (idx + 1)
+                  last_baby_count = baby[:count]
+                end
+                # calculate per_100k
+                baby[:per_100k] = (baby[:count] * 100000.0 / total_babies_of_sex).round
+                # if previous year data exists, calculate the change
+                if (p_baby = prev_year_data[[baby[:name], baby[:sex]]])
+                  baby[:count_change] = baby[:count] - p_baby[:count]
+                  baby[:per_100k_change] = baby[:per_100k] - p_baby[:per_100k]
+                  baby[:rank_change] = p_baby[:rank] - baby[:rank]
+                else
+                  baby[:count_change] = baby[:count]
+                  baby[:per_100k_change] = baby[:per_100k]
+                  # need to invert this number by total_names
+                  baby[:rank_change] = (total_names_of_sex - baby[:rank]) + 1
+                end
+                # write to CSV
+                csv << baby
+                current_year_data[[baby[:name], baby[:sex]]] = baby
+              end # done with this sex
+            end # done with this year / fname
+          end
+          prev_year_data  = current_year_data
+        end
+
+        csv.close
+        true
+      end
+    end
+
+  end
 end
+
